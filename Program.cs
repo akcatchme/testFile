40a475e8a7a8e3ba2b5f0714229de77605df6c5b
Anil <anil.khadka0@gmail.com>
2022-07-10 16:04:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp1CopyFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string zipFullPath = @"C:\Users\sonyl\source\repos\Zip\TestFolder.zip";
+
+            string unzip = @"C:\Users\sonyl\source\repos\unzip\";
+
+            string policies = @"C:\Users\sonyl\source\repos\policies\";
+
+            string processed = @"C:\Users\sonyl\source\repos\processed\";
+
+
+            if (!Directory.Exists(unzip))
+            {
+                Directory.CreateDirectory(unzip);
+            }
+
+            var unzipFolderName = Path.GetFileNameWithoutExtension(zipFullPath);
+            string unzipIndividualFolder = Path.Combine(unzip, unzipFolderName);
+
+            Directory.Delete(unzipIndividualFolder, true);
+            Directory.CreateDirectory(unzipIndividualFolder);
+
+
+            using (ZipArchive arch = ZipFile.OpenRead(zipFullPath))
+            {
+
+                foreach (ZipArchiveEntry entry in arch.Entries)
+                {
+                    string fileFullPath = Path.Combine(unzipIndividualFolder, entry.FullName);
+
+                    string pathWithOutFileName = Path.GetDirectoryName(fileFullPath);
+
+                    if (!Directory.Exists(pathWithOutFileName))
+                        Directory.CreateDirectory(pathWithOutFileName);
+
+                    if (!string.IsNullOrEmpty(entry.Name))
+                        entry.ExtractToFile(Path.Combine(unzipIndividualFolder, entry.FullName));
+                }
+            }
+
+            if (!Directory.Exists(processed))
+            {
+                Directory.CreateDirectory(processed);
+            }
+
+
+            File.Move(zipFullPath, Path.Combine(processed, Path.GetFileName(zipFullPath)));
+
+
+
+            if (!Directory.Exists(policies))
+            {
+                Directory.CreateDirectory(policies);
+            }
+
+
+            string[] filePaths = Directory.GetFiles(unzipIndividualFolder, "*.*", SearchOption.AllDirectories);
+
+            foreach (var filepath in filePaths)
+            {
+
+                string newFilePath = filepath.Replace(unzip, policies); // Path.Combine(policies, Path.GetFileName(filepath));
+
+                if (!File.Exists(newFilePath))
+                {
+                    string pathWithOutFileName = Path.GetDirectoryName(newFilePath);
+
+                    if (!Directory.Exists(pathWithOutFileName))
+                        Directory.CreateDirectory(pathWithOutFileName);
+
+                    File.Copy(filepath, newFilePath, true);
+                }
+            }
+        }
+    }
+}
